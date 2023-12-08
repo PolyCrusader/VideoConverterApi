e4ee0e42f4e0b89793e82b87ce9811540684f2bd
eymeric <eymericdechelette@gmail.com>
2023-12-08 01:09:14 +0100

    v1

--- index.ts
+++ index.ts
@@ -1,15 +1,12 @@
 import express, { Request, Response, Application } from 'express';
-import ffmpeg from 'ffmpeg-static';
 import ffprobe from 'ffprobe-static';
 import dotenv from 'dotenv';
 import child_process from 'child_process';
 import fs from 'fs';
-import urlParser from 'url';
-import ffmpegPath from 'ffmpeg-static';
+import ffmpeg from 'ffmpeg-static';
 import got from 'got';
 import stream from 'stream';
 import util from 'util';
-import optimizeVideo from './video';
 
 //For env File 
 dotenv.config();
@@ -37,84 +34,114 @@ app.post('/', async (req: Request, res: Response) => {
 
   const originalVideoInfoJson = JSON.parse(child_process.execSync(`${ffprobe.path} -v error -show_format -show_streams -of json uploads/${input_file}`).toString());
 
-  //CRADO faire avec de la reocnnaissance d'extension on avec file
   let input_streams = originalVideoInfoJson.streams.map((stream: any) => stream.codec_type).join("\n");
   let duration: string = originalVideoInfoJson.format.duration;
 
   let output_format: string = "";
   let output_codec: string = "";
-  let intput_duration: number;
+  let input_duration: number;
   if (duration == undefined) {
     output_format = ".webp";
     output_codec = "libwebp";
-    intput_duration = 1;
+    input_duration = 1;
   } else {
-    intput_duration = parseFloat(duration.toString());
+    input_duration = parseFloat(duration.toString());
     output_format = ".mp4";
     output_codec = "h264";
   }
 
-  /*
-  //get 10sec of the video
-  let short_duration = 10;
-  if (intput_duration < short_duration) {
-    short_duration = intput_duration;
+  if (final_size * 8 / input_duration < 100) { //TODO maybe change the value
+    res.json({ error: "maxSize is too small" });
+    return;
   }
-
-  const short_file = `${input_file.split('.').slice(0, -1).toString()}_short${output_format}`;
-  const tmp_generation_file = `${input_file.split('.').slice(0, -1).toString()}_test${output_format}`;
-  // ffmpeg -i test.mp4 -ss 0 -t 600 -c copy  test_output.mp4
-  if (short_duration > 1) {
-    child_process.execSync(`${ffmpegPath} -i uploads/${input_file} -ss 0 -t ${short_duration} -c copy tmp_generations/${short_file}`);
-  } else {
-    fs.copyFileSync(`uploads/${input_file}`, `tmp_generations/${short_file}`);
-  }
-  const p = 0.0007;
-  const d = 0.0001;
-  let outputBitrate = (final_size * 8) / intput_duration;
-  let short_file_size = (final_size * 1024) / intput_duration * short_duration;
-  console.log("short_file_size", short_file_size);
-  let test_file_size = short_file_size + 1;
-  let last_test_file_size = test_file_size;
-  let counter = 0;
-  while (test_file_size - short_file_size > 0) {
-    if (counter > 20) {
-      res.json({ error: "can't find a bitrate" });
-      return;
-    }
-    console.log("outputBitrate", outputBitrate);
-    child_process.execSync(`${ffmpegPath} -i tmp_generations/${short_file} -y -b:v ${outputBitrate}k -vcodec ${output_codec} tmp_generations/${tmp_generation_file}`);
-    test_file_size = fs.statSync(`tmp_generations/${tmp_generation_file}`).size;
-    outputBitrate = Math.abs(outputBitrate - Math.abs(short_file_size - test_file_size) * p - Math.abs(last_test_file_size - test_file_size) * d);
-    counter += 1;
-  }
-  //clean files
-  fs.unlinkSync(`tmp_generations/${short_file}`);
-  fs.unlinkSync(`tmp_generations/${tmp_generation_file}`);
-  */
 
   const output_file = `${input_file.split('.').slice(0, -1).toString()}${output_format}`;
-  const { bitrate: outputBitrate, width: outputWith, height: outputHeight } = optimizeVideo(originalVideoInfoJson, final_size);
-  //TODO remove the -y
-  let command: string = `${ffmpegPath} -i uploads/${input_file} -y -b:v ${outputBitrate} -vcodec ${output_codec} -vf scale=${outputWith}:${outputHeight},fps=25 outputs/${output_file}`;
-  console.log(command);
 
-  child_process.execSync(
-    command,
-  );
-  fs.unlinkSync(`uploads/${input_file}`);
+  //TODO replace with real ip
   res.json({ "result": "success", "url": `http://localhost:${port}/outputs/${output_file}` });
+  if (duration == undefined) {
+    processImage(input_file, originalVideoInfoJson, final_size, input_duration, output_file);
+  } else {
+    processVideo(input_file, originalVideoInfoJson, final_size, input_duration, output_file);
+  }
+  deleteOldFiles();
 });
 
 app.get('/outputs/:file', (req: Request, res: Response) => {
   const file = req.params.file;
   const fileLocation = `outputs/${file}`;
+  if (fs.existsSync(`progress/${file}-1.txt`) || fs.existsSync(`progress/${file}-2.txt`)) {
+    const progress = getProgress(file);
+    res.json({ "result": "error", "error": "encoding not finished", "avencement": progress });
+    return;
+  }
   res.download(fileLocation, file);
 });
 
-app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
-});
+function processVideo(input_file: string, originalVideoInfoJson: any, final_size: number, input_duration: number, output_file: string) {
+  const outputBitrate = (final_size * 8) / originalVideoInfoJson.format.duration * (80 / 100);
+  //TODO remove the -y
+  let command: string = `${ffmpeg} -y -i uploads/${input_file} -c:v libx264 -b:v ${outputBitrate}k -pass 1 -an -progress progress/${output_file}-1.txt -f null /dev/null && \
+  ${ffmpeg} -y -i uploads/${input_file} -c:v libx264 -b:v ${outputBitrate}k -pass 2 -progress progress/${output_file}-2.txt outputs/${output_file}`;
+  fs.writeFileSync(`progress/${output_file}-duration.txt`, input_duration.toString());
+  child_process.exec(
+    command,
+  ).on('exit', function (code, signal) {
+    console.log('child process exited');
+    fs.unlinkSync(`uploads/${input_file}`);
+    fs.unlinkSync(`progress/${output_file}-1.txt`);
+    fs.unlinkSync(`progress/${output_file}-2.txt`);
+    fs.unlinkSync(`progress/${output_file}-duration.txt`);
+  });
+}
+
+function processImage(input_file: string, originalVideoInfoJson: any, final_size: number, input_duration: number, output_file: string) {
+  let outputSize: number = 0;
+  let height: number = originalVideoInfoJson.streams[0].height;
+  let width: number = originalVideoInfoJson.streams[0].width;
+  do {
+    let command: string = `${ffmpeg} -y -i uploads/${input_file} -vf scale=${width}:${height} -c:v libwebp -progress progress/${output_file}-1.txt tmp_generations/${output_file}`;
+    child_process.execSync(command);
+    outputSize = fs.statSync(`tmp_generations/${output_file}`).size / 1024;
+    const factor: number = final_size / outputSize;
+    if (factor < 1) {
+      height = Math.round(height * factor);
+      width = Math.round(width * factor);
+      if (height % 2 !== 0) {
+        height -= 1;
+      }
+      if (width % 2 !== 0) {
+        width -= 1;
+      }
+
+    }
+  } while (outputSize > final_size);
+  let command = `${ffmpeg} -y -i tmp_generations/${output_file} -vf scale=${width}:${height} -c:v libwebp -progress progress/${output_file}-2.txt outputs/${output_file}`;
+  child_process.execSync(command);
+  fs.unlinkSync(`progress/${output_file}-2.txt`);
+  fs.unlinkSync(`progress/${output_file}-1.txt`);
+  fs.unlinkSync(`tmp_generations/${output_file}`);
+  fs.unlinkSync(`uploads/${input_file}`);
+}
+
+function getProgress(file: string): number {
+  let offset: number;
+  let out_time_ms: string;
+  if (fs.existsSync(`progress/${file}-2.txt`)) {
+    offset = 50;
+    const data = fs.readFileSync(`progress/${file}-2.txt`, 'utf8');
+    out_time_ms = data.split("\n").filter((line: string) => line.startsWith("out_time_ms")).slice(-1).toString().split("=").slice(-1).toString();
+  } else {
+    offset = 0;
+    const data = fs.readFileSync(`progress/${file}-1.txt`, 'utf8');
+    out_time_ms = data.split("\n").filter((line: string) => line.startsWith("out_time_ms")).slice(-1).toString().split("=").slice(-1).toString();
+  }
+  let duration = fs.readFileSync(`progress/${file}-duration.txt`, 'utf8');
+  if (duration == undefined) {//this is probably an image
+    duration = "1";
+  }
+  return (parseFloat(out_time_ms) / 1000000) / parseFloat(duration) * 50 + offset;
+}
 
 function makeid(length: number) {
   let result = '';
@@ -127,3 +154,21 @@ function makeid(length: number) {
   }
   return result;
 }
+
+function deleteOldFiles() {
+  const files = fs.readdirSync("outputs");
+  const now = new Date().getTime();
+  files.forEach((file) => {
+    const fileLocation = `outputs/${file}`;
+    const stats = fs.statSync(fileLocation);
+    const endTime = new Date(stats.mtime).getTime() + 1000 * 60 * 60 * 24 * 7; //7 days
+    if (now > endTime) {
+      fs.unlinkSync(fileLocation);
+    }
+  });
+
+}
+
+app.listen(port, () => {
+  console.log(`Server is Fire at http://localhost:${port}`);
+});
